b3770dd9601f008aff0a9b66c7769fe6daf37509
Lara <dmilarisa@gmail.com>
2025-02-13 23:24:20 +0000

    delete unuseful imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Weather from "./Weather"
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
         <footer>
           <a
             href="https://github.com/dmilarisa/weather-react-app"
@@ -17,7 +17,11 @@ export default function App() {
             Open-sourse code
           </a>
           &nbsp; by &nbsp;
-          <a href="" target="_blank" rel="noopener noreferrer">
+          <a
+            href="www.linkedin.com/in/larysa-dmytrenko-04455379"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Larysa Dmytrenko
           </a>
         </footer>
